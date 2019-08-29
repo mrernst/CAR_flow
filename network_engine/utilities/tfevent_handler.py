b6d5be8cad5c897f64a0a04fc6deb591547dd615
Markus Ernst <markusrolandernst@t-online.de>
2019-08-29 11:58:30 +0200

    deprecation

--- network_engine/utilities/tfevent_handler.py
+++ network_engine/utilities/tfevent_handler.py
@@ -194,18 +194,21 @@ def variable_summaries(var, name, weights=False):
     summaries = []
     with tf.name_scope('summaries_{}'.format(name)):
         mean = tf.reduce_mean(var)
-        summaries.append(tf.summary.scalar('mean', mean))
+        summaries.append(tf.compat.v1.summary.scalar('mean', mean))
         with tf.name_scope('stddev'):
             stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        summaries.append(tf.summary.scalar('stddev', stddev))
-        summaries.append(tf.summary.scalar('max', tf.reduce_max(var)))
-        summaries.append(tf.summary.scalar('min', tf.reduce_min(var)))
+        summaries.append(tf.compat.v1.summary.scalar(
+            'stddev', stddev))
+        summaries.append(tf.compat.v1.summary.scalar(
+            'max', tf.reduce_max(var)))
+        summaries.append(tf.compat.v1.summary.scalar(
+            'min', tf.reduce_min(var)))
         if weights:
             # exitatory = tf.nn.relu(var)
             # inhibitory = tf.nn.relu(tf.negative(var))
             # summaries += variable_summaries(exitatory, name + '/exitatory')
             # summaries += variable_summaries(inhibitory, name + '/inhibitory')
-            summaries.append(tf.summary.scalar('norm', tf.norm(var)))
+            summaries.append(tf.compat.v1.summary.scalar('norm', tf.norm(var)))
             # summaries.append(tf.summary.histogram('histogram', var))
     return summaries
 
@@ -215,7 +218,7 @@ def module_scalar_summary(module):
     module_scalar_summary takes a module as its argument and returns a list
     of summary objects to be added to a list or merged
     """
-    return [tf.summary.scalar(module.name + "_{}".format(timestep),
+    return [tf.compat.v1.summary.scalar(module.name + "_{}".format(timestep),
             module.outputs[timestep]) for timestep in module.outputs]
 
 
