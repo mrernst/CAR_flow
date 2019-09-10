2615b65b85e7835f1093f79f07fe334fdf13eef0
Markus Ernst <markusrolandernst@t-online.de>
2019-09-10 13:22:10 +0200

    cleanup

--- network_engine/utilities/networks/preprocessor.py
+++ network_engine/utilities/networks/preprocessor.py
@@ -209,57 +209,6 @@ class PreprocessorNetwork(bb.ComposedModule):
             self.input_module = self.layers["pass_through"]
             self.output_module = self.layers["inp_norm"]
 
-
-# TODO: Overwrite init function to have variables for the input Statistics
-# TODO: Write Method that calcs those statistics given a session and filenames
-
-# code from main engine:
-#
-#
-# #input statistics for normalization
-# self.stats = {}
-# self.update_stats = {}
-# self.reset_stats = {}
-#
-# # not initialized to zero for graceful error handling
-# self.stats['N'] = tf.Variable(1., trainable=False)
-# self.stats['Sx'] = tf.Variable(tf.zeros([1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS]), trainable=False)
-# self.stats['Sxx'] = tf.Variable(tf.ones([1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS]), trainable=False)
-#
-# self.update_stats['N'] = tf.compat.v1.assign_add(self.stats['N'], BATCH_SIZE)
-# self.update_stats['Sx'] = tf.compat.v1.assign_add(self.stats['Sx'], tf.expand_dims(tf.reduce_sum(tf.cast(inp.variable, tf.float32), axis=0), 0))
-# self.update_stats['Sxx'] = tf.compat.v1.assign_add(self.stats['Sxx'], tf.expand_dims(tf.reduce_sum(tf.square(tf.cast(inp.variable, tf.float32)), axis=0), 0))
-#
-# self.reset_stats['N'] = tf.compat.v1.assign(self.stats['N'], 0)
-# self.reset_stats['Sx'] = tf.compat.v1.assign(self.stats['Sx'], tf.zeros([1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS]))
-# self.reset_stats['Sxx'] = tf.compat.v1.assign(self.stats['Sxx'], tf.zeros([1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS]))
-# ----
-#
-#
-# def gather_input_stats(flnames=training_filenames):
-#     sess.run(iterator.initializer, feed_dict={filenames: flnames})
-#     print(" " * 80 + "\r" + "[Statistics]\tstarted", end="\r")
-#     sess.run([self.reset_stats['N'],
-#               self.reset_stats['Sx'], self.reset_stats['Sxx']])
-#     while True:
-#         try:
-#             N, Sx, Sxx = sess.run([self.update_stats['N'], self.update_stats['Sx'], self.update_stats['Sxx']], feed_dict={
-#                                   is_training.placeholder: False, keep_prob.placeholder: 1.0})
-#         except (tf.errors.OutOfRangeError):
-#             sess.run([tf.compat.v1.assign(network.layers['pw_norm'].n, self.stats['N']),
-#                       tf.compat.v1.assign(
-#                           network.layers['pw_norm'].sx, self.stats['Sx']),
-#                       tf.compat.v1.assign(network.layers['pw_norm'].sxx, self.stats['Sxx'])])
-#             # import matplotlib.pyplot as plt
-#             # a = sess.run(network.layers['pw_norm'].outputs[0], feed_dict = {is_training.placeholder:False, keep_prob.placeholder: 1.0})
-#             # for i in range(50):
-#             #   #plt.imshow(batch[0][i,:,:,0])
-#             #   plt.imshow(a[i,:,:,0])
-#             #   plt.show()
-#             break
-#         pass
-
-
 # _____________________________________________________________________________
 
 
