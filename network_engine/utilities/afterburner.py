d6e9d893db52b8dd6138a18e5ddbf260a1b25614
Markus Ernst <markusrolandernst@t-online.de>
2019-10-01 13:20:45 +0200

    plotting

--- network_engine/utilities/afterburner.py
+++ network_engine/utilities/afterburner.py
@@ -48,6 +48,7 @@ import tensorflow as tf
 import numpy as np
 import pickle
 import os
+from matplotlib import pyplot as plt
 
 # custom functions
 # -----
@@ -80,17 +81,141 @@ class DataEssence(object):
 
     def distill(self, path, evaluation_data, embedding_data=None):
         self.essence = self._read_tfevents(path)
+
+        needless_keys = [l for l in list(self.essence.keys()) if '/' in l]
+        for key in needless_keys:
+            self.essence.pop(key, None)
+
         self.essence['evaluation'] = evaluation_data
         if embedding_data:
-            # TODO: implement embedding data
-            pass
+            self.essence['embedding'] = embedding_data
 
     def _read_tfevents(self, path):
         em = tfevent_handler.read_multiple_runs(path)
         df = tfevent_handler.convert_em_to_df(em)
         return df
 
-    def plot_essentials(self, path):
+    def plot_essentials(self, savefile):
+        # start figure
+        fig, axes = plt.subplots(4, 3, sharex='all', figsize=[7, 9])
+        # plot images onto figure
+        self._plot_traintest_lcurve(axes)
+        self._plot_timebased_lcurve(axes)
+        self._plot_parameter_lcurve(fig)
+
+        # save figure
+        fig.suptitle(savefile.rsplit('/')[-1])
+        fig.savefig(savefile)
+        pass
+
+    def _plot_traintest_lcurve(self, axes):
+        training_acc = \
+            self.essence['training']['accuracy_and_error/accuracy'].tolist()
+        training_partacc = \
+            self.essence['training']['accuracy_and_error/partial_accuracy']\
+            .tolist()
+        training_loss = \
+            self.essence['training']['accuracy_and_error/loss'].tolist()
+        training_steps = \
+            self.essence['training']['step'].tolist()
+
+        testing_acc = \
+            self.essence['testing']['testtime/accuracy'].tolist()
+        testing_partacc = \
+            self.essence['testing']['testtime/partial_accuracy'].tolist()
+        testing_loss = \
+            self.essence['testing']['testtime/loss'].tolist()
+        testing_steps = \
+            self.essence['testing']['step'].tolist()
+
+        axes[0, 0].plot(training_steps, training_acc)
+        axes[0, 0].plot(testing_steps, testing_acc)
+        axes[0, 0].set_title('accuracy')
+
+        axes[0, 2].plot(training_steps, training_partacc)
+        axes[0, 2].plot(testing_steps, testing_partacc)
+        axes[0, 2].set_title('partial accuracy')
+
+        axes[0, 1].plot(training_steps, training_loss)
+        axes[0, 1].plot(testing_steps, testing_loss)
+        axes[0, 1].set_title('loss')
+
+        pass
+
+    def _plot_timebased_lcurve(self, axes):
+        sorted_list_of_keys = self.essence['testing'].keys().tolist()
+        sorted_list_of_keys.sort()
+        timedepth = int(sorted_list_of_keys[-1].rsplit('_')[-1])
+
+        sorted_list_of_keys = \
+            [key for key in sorted_list_of_keys if 'testtime_beyond' in key]
+        sorted_list_of_keys = \
+            [key.rsplit('_', 1)[0] for key in sorted_list_of_keys]
+        sorted_list_of_keys = list(set(sorted_list_of_keys))
+        sorted_list_of_keys.sort()
+        for i in range(len(sorted_list_of_keys)):
+            for j in range(timedepth+1):
+                axes[1, i].plot(self.essence['testing']['step'].tolist(),
+                                self.essence['testing']['{}_{}'.format(
+                                    sorted_list_of_keys[i], j)].tolist(),
+                                label=sorted_list_of_keys[i].split('/')[-1])
+            axes[1, i].set_title(sorted_list_of_keys[i].split('/')[-1])
+        pass
+
+    def _plot_parameter_lcurve(self, fig):
+        sorted_list_of_keys = self.essence['training'].keys().tolist()
+        sorted_list_of_keys.sort()
+        sorted_list_of_keys = \
+            [key for key in sorted_list_of_keys if 'weights_and_biases' in key]
+        sorted_list_of_keys = \
+            [key.rsplit('/', 1)[0] for key in sorted_list_of_keys]
+        sorted_list_of_keys = list(set(sorted_list_of_keys))
+        sorted_list_of_keys = \
+            [key for key in sorted_list_of_keys if '_weights' in key]
+        sorted_list_of_keys.sort()
+        # for i in range(len(sorted_list_of_keys)):
+        for i, ax in enumerate(fig.axes[6:]):
+            if i < len(sorted_list_of_keys):
+                ax.plot(
+                    self.essence['training']['step'].tolist(),
+                    self.essence['training']['{}/mean_1'.format(
+                        sorted_list_of_keys[i])].tolist(),
+                    label=sorted_list_of_keys[i].split('/')[1],
+                    color='blue')
+                ax.plot(
+                    self.essence['training']['step'].tolist(),
+                    self.essence['training']['{}/min_1'.format(
+                        sorted_list_of_keys[i])].tolist(),
+                    label=sorted_list_of_keys[i].split('/')[1],
+                    color='blue')
+                ax.plot(
+                    self.essence['training']['step'].tolist(),
+                    self.essence['training']['{}/max_1'.format(
+                        sorted_list_of_keys[i])].tolist(),
+                    label=sorted_list_of_keys[i].split('/')[1],
+                    color='blue')
+                ax.fill_between(
+                    self.essence['training']['step'],
+                    self.essence['training']['{}/mean_1'.format(
+                        sorted_list_of_keys[i])] +
+                    self.essence['training']['{}/stddev_1'.format(
+                        sorted_list_of_keys[i])],
+                    self.essence['training']['{}/mean_1'.format(
+                        sorted_list_of_keys[i])] -
+                    self.essence['training']['{}/stddev_1'.format(
+                        sorted_list_of_keys[i])],
+                    color='blue',
+                    alpha=0.3
+                )
+                ax.set_title(
+                    sorted_list_of_keys[i].split('/')[1].split('_')[1])
+        pass
+
+    # TODO: write these visualizer functions
+    def visualize_softmax_output(self):
+        pass
+
+    def vizualize_tsne_analysis(self):
         pass
 
 
@@ -122,7 +247,10 @@ class EssenceCollection(object):
         with open(filename, 'rb') as input:
             self.essence = pickle.load(input)
 
-    def plot_essentials(self):
+    def plot_essentials(self, filename):
+        pass
+
+    def _plot_barplot_comparison(self):
         pass
 
 
