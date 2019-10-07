cfcaecd61df59e4b8c32c37e7938d60252372bd7
Markus Ernst <markusrolandernst@t-online.de>
2019-10-07 16:15:15 +0200

    beautification

--- network_engine/utilities/afterburner.py
+++ network_engine/utilities/afterburner.py
@@ -163,7 +163,7 @@ class DataEssence(object):
         sorted_list_of_keys = list(set(sorted_list_of_keys))
         sorted_list_of_keys.sort()
         for i in range(len(sorted_list_of_keys)):
-            for j in range(timedepth+1):
+            for j in range(timedepth + 1):
                 axes[1, i].plot(self.essence['testing']['step'].tolist(),
                                 self.essence['testing']['{}_{}'.format(
                                     sorted_list_of_keys[i], j)].tolist(),
@@ -230,6 +230,7 @@ class DataEssence(object):
 
 class EssenceCollection(object):
     """docstring for EssenceCollection."""
+
     def __init__(self, remove_files=False):
         super(EssenceCollection, self).__init__()
         path_to_file = os.path.realpath(__file__)
@@ -269,7 +270,7 @@ class EssenceCollection(object):
             self.collection = pickle.load(input)
 
     def plot_barplot_comparison(self, filename):
-        plt.imshow(np.random.random([100,100]))
+        plt.imshow(np.random.random([100, 100]))
         plt.savefig(filename)
         pass
 
@@ -281,30 +282,33 @@ class EssenceCollection(object):
 
         rects = []
         n_groups = len(self.collection.keys())
-        n_iterations = 100 # high number so that true value is smaller
+        n_iterations = 100  # high number so that true value is smaller
         for configuration in self.collection.keys():
-            n_iterations = min(n_iterations, len(self.collection[configuration].keys()))
+            n_iterations = min(n_iterations,
+                               len(self.collection[configuration].keys()))
 
         n_bars = 1
         data = np.zeros([n_iterations, n_bars, n_groups])
         means = np.zeros([n_bars, n_groups])
         stderror = np.zeros([n_bars, n_groups])
         mcnemar_data = {}
-        evaluation_iteration = 1
+        ev_it = 1
 
         list_of_configurations = list(self.collection.keys())
         list_of_configurations.sort()
 
-
-        for j,configuration in enumerate(list_of_configurations):
-            list_of_iterations = list(self.collection['config{}'.format(j)].keys())
+        for j, configuration in enumerate(list_of_configurations):
+            list_of_iterations = list(
+                self.collection['config{}'.format(j)].keys())
             list_of_iterations.sort()
 
             for it, iteration in enumerate(list_of_iterations):
-                data[it,0,j] = 1 - self.collection[configuration][iteration]['testing']['testtime/partial_accuracy'].values[-1]
-                mcnemar_data[it,0,j] = self.collection[configuration][iteration]['evaluation']['boolean_classification']
+                data[it, 0, j] = 1 - \
+                    self.collection[configuration][iteration]['testing']['testtime/partial_accuracy'].values[-1]
+                mcnemar_data[it, 0, j] = \
+                    self.collection[configuration][iteration]['evaluation']['boolean_classification']
 
-        chancelevel = 1 - 1./10.
+        chancelevel = 1 - 1. / 10.
 
         means = np.mean(data, axis=0)
         stderror = np.std(data, axis=0) / np.sqrt(n_iterations)
@@ -314,8 +318,7 @@ class EssenceCollection(object):
         print((means).round(3))
         print((stderror).round(3))
 
-
-        fig, ax = plt.subplots(figsize=[8.0/6*n_groups, 6.0])
+        fig, ax = plt.subplots(figsize=[8.0 / 6 * n_groups, 6.0])
 
         index = np.arange(n_groups)
         bar_width = 0.8
@@ -323,139 +326,166 @@ class EssenceCollection(object):
         opacity = 1
         error_config = {'ecolor': '0.3'}
 
-        current_palette = sns.dark_palette(sns.color_palette('colorblind')[0], 5, reverse=True)
-        current_palette = sns.dark_palette(sns.color_palette('Set3')[0], 5, reverse=True)
-        current_palette = sns.dark_palette('LightGray', 5, reverse=True)
-        current_palette = sns.dark_palette('Black', 5, reverse=False)
-
+        current_palette = sns.dark_palette(
+            sns.color_palette('colorblind')[0], 5, reverse=True)
+        current_palette = sns.dark_palette(
+            sns.color_palette('Set3')[0], 5, reverse=True)
+        current_palette = sns.dark_palette(
+            'LightGray', 5, reverse=True)
+        current_palette = sns.dark_palette(
+            'Black', 5, reverse=False)
 
         for k in range(n_bars):
-            rects.append(ax.bar(index + k*bar_width, means[k], bar_width,
-                         alpha=opacity,
-                         yerr=stderror[k], error_kw=error_config,
-                         label='{}'.format('configuration'), linewidth=0, color=current_palette[k])) #, edgecolor=current_palette[(k+1)], hatch=hatches[k]))
-
-
+            rects.append(ax.bar(index + k * bar_width, means[k], bar_width,
+                                alpha=opacity,
+                                yerr=stderror[k], error_kw=error_config,
+                                label='{}'.format('configuration'),
+                                linewidth=0, color=current_palette[k]))
+            # , edgecolor=current_palette[(k+1)],
+            # hatch=hatches[k]))
 
         ax.set_xlabel('Network configuration')
         ax.set_ylabel('Error rate')
         ax.set_title('experiment: {}'.format(
             self.path_to_experiment.rsplit('/')[-1]), fontsize=10)
-        ax.set_ylim([0,ax.get_ylim()[1]*1])  # *1.25])
-        ax.set_xticks(index + bar_width * (n_bars/2. - 0.5))
+        ax.set_ylim([0, ax.get_ylim()[1] * 1])  # *1.25])
+        ax.set_xticks(index + bar_width * (n_bars / 2. - 0.5))
         # ax.set_xticklabels([arch[:-1] for arch in ARCHARRAY])
-        ax.legend(frameon=True, facecolor='white', edgecolor='white', framealpha=1.0)
+        ax.legend(frameon=True, facecolor='white',
+                  edgecolor='white', framealpha=1.0)
         ax.grid(axis='y', zorder=0, alpha=0.5)
         ax.set_axisbelow(True)
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
         ax.tick_params(
-        axis='x',          # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom=False,      # ticks along the bottom edge are off
-        top=False,         # ticks along the top edge are off
-        labelbottom=True) # labels along the bottom edge are off
+            axis='x',          # changes apply to the x-axis
+            which='both',      # both major and minor ticks are affected
+            bottom=False,      # ticks along the bottom edge are off
+            top=False,         # ticks along the top edge are off
+            labelbottom=True)  # labels along the bottom edge are off
         ax.spines['left'].set_visible(False)
 
         ax.axhline(y=chancelevel, xmin=0, xmax=6, color='black')
-        ax.text(0.2, chancelevel+0.005, r'chancelevel')
+        ax.text(0.2, chancelevel + 0.005, r'chancelevel')
 
         # Shrink current axis by 20%
         box = ax.get_position()
         ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
         # Put a legend to the right of the current axis
-        ax.legend(frameon=True, facecolor='white', edgecolor='white', framealpha=1.0, bbox_to_anchor=(1, .9), loc='center left')
+        ax.legend(frameon=True, facecolor='white', edgecolor='white',
+                  framealpha=1.0, bbox_to_anchor=(1, .9), loc='center left')
 
-        #mcnemar's table
+        # mcnemar's table
         for i in range(n_bars):
             qstar = 0.05
-            pval = np.ones([n_groups*n_bars, n_groups*n_bars])
-            chi_table = np.ones([n_groups*n_bars, n_groups*n_bars])
-            significance_table = np.zeros([n_groups*n_bars, n_groups*n_bars])
+            pval = np.ones([n_groups * n_bars, n_groups * n_bars])
+            chi_table = np.ones([n_groups * n_bars, n_groups * n_bars])
+            significance_table = np.zeros(
+                [n_groups * n_bars, n_groups * n_bars])
             vertexlist = []
-            for k in range(n_groups*n_bars):
-                for j in range(n_groups*n_bars):
-                    if k!=j and k>j:
-                    #print(i,j,k,mcnemar_data[(evaluation_iteration-1),i,k].shape, mcnemar_data[(evaluation_iteration-1),i,j].shape)
-                        mcnemar_table = 2*mcnemar_data[(evaluation_iteration-1),i,k] -  mcnemar_data[(evaluation_iteration-1),i,j]
-                        if (len(mcnemar_data[(evaluation_iteration-1),i,k].shape) > 1):  # i.e. nhot encoding
-                            ak = np.sum(np.array([e for e in mcnemar_table == 1.]), axis=1)
-                            bk = np.sum(np.array([e for e in mcnemar_table == 2.]), axis=1)
-                            ck = np.sum(np.array([e for e in mcnemar_table == -1.]), axis=1)
-                            dk = np.sum(np.array([e for e in mcnemar_table == 0]), axis=1)
-                            chi_sq_denom = np.sum(((bk - ck)/mcnemar_table.shape[-1]))**2
-                            chi_sq_count = np.sum(((bk - ck)/mcnemar_table.shape[-1])**2)
-                            chi_sq = chi_sq_denom/chi_sq_count
+            for k in range(n_groups * n_bars):
+                for j in range(n_groups * n_bars):
+                    if k != j and k > j:
+                        mcnemar_table = 2 * \
+                            mcnemar_data[(ev_it - 1), i, k] - \
+                            mcnemar_data[(ev_it - 1), i, j]
+                        # i.e. nhot encoding
+                        if (len(mcnemar_data[(ev_it - 1), i, k].shape) > 1):
+                            ak = np.sum(
+                                np.array(
+                                    [e for e in mcnemar_table == 1.]), axis=1)
+                            bk = np.sum(
+                                np.array(
+                                    [e for e in mcnemar_table == 2.]), axis=1)
+                            ck = np.sum(
+                                np.array(
+                                    [e for e in mcnemar_table == -1.]), axis=1)
+                            dk = np.sum(
+                                np.array(
+                                    [e for e in mcnemar_table == 0]), axis=1)
+                            chi_sq_denom = np.sum(
+                                ((bk - ck) / mcnemar_table.shape[-1]))**2
+                            chi_sq_count = np.sum(
+                                ((bk - ck) / mcnemar_table.shape[-1])**2)
+                            chi_sq = chi_sq_denom / chi_sq_count
                         else:
                             a = mcnemar_table[mcnemar_table == 1.].shape[0]
                             b = mcnemar_table[mcnemar_table == 2.].shape[0]
                             c = mcnemar_table[mcnemar_table == -1.].shape[0]
                             d = mcnemar_table[mcnemar_table == 0].shape[0]
-                            chi_sq = ((b-c)**2)/(b+c)
-                        pval[k,j] = 1 - stats.chi2.cdf(chi_sq, 1) #stats.chi2.pdf(chi_sq , 1) #
-                        chi_table[k,j] = chi_sq
+                            chi_sq = ((b - c)**2) / (b + c)
+                        # stats.chi2.pdf(chi_sq , 1) #
+                        pval[k, j] = 1 - stats.chi2.cdf(chi_sq, 1)
+                        chi_table[k, j] = chi_sq
 
+            print(np.round(pval, 4))
+            print(np.round(chi_table, 4))
+            sorted_pvals = np.sort(pval[pval < 1])
+            bjq = np.arange(1, len(sorted_pvals) + 1) / \
+                len(sorted_pvals) * qstar
 
-            print(np.round(pval,4))
-            print(np.round(chi_table,4))
-            sorted_pvals = np.sort(pval[pval<1])
-            bjq = np.arange(1,len(sorted_pvals)+1)/len(sorted_pvals)*qstar
+            for k in range(n_groups * n_bars):
+                for j in range(n_groups * n_bars):
+                    if k != j and k > j:
+                        if pval[k, j] in sorted_pvals[sorted_pvals - bjq <= 0]:
+                            significance_table[k, j] = 1
 
-            for k in range(n_groups*n_bars):
-                for j in range(n_groups*n_bars):
-                    if k!=j and k>j:
-                        if pval[k,j] in sorted_pvals[sorted_pvals - bjq <= 0]:
-                            significance_table[k,j] = 1
-
-                            if j<3 and k>2 and (means[i][j] < means[i][k]):
-                                vertexlist.append([[j+0.25,k-0.5],[j+0.5,k-0.25],[j+0.5,k-0.5]])
+                            if j < 3 and k > 2 and (means[i][j] < means[i][k]):
+                                vertexlist.append(
+                                    [[j + 0.25, k - 0.5],
+                                     [j + 0.5, k - 0.25],
+                                     [j + 0.5, k - 0.5]])
                         else:
-                            significance_table[k,j] = 0
-
+                            significance_table[k, j] = 0
 
             ax = plt.axes([0.8, 0.3, .2, .2])
             ax.matshow(significance_table, cmap='Greys')
-            #ax.set_title("McNemar's Test {} {}".format(PERCENTAGE[i],h))
-            #ax.set_xticklabels([''] + [arch[:-1] for arch in ARCHARRAY], fontsize=8)#fontsize=65)
-            ax.tick_params(labelbottom='on',labeltop='off', top='off', right='off')
-            #ax.set_yticklabels([arch[:-1] for arch in ARCHARRAY], fontsize=8) #fontsize=65)
-            ax.set_xlim([-0.5, n_groups*n_bars -1-0.5])
-            ax.set_ylim([n_groups*n_bars-0.5, -0.5+1])
+            # ax.set_xticklabels(, fontsize=8)#fontsize=65)
+            ax.tick_params(labelbottom='on', labeltop='off',
+                           top='off', right='off')
+            # ax.set_yticklabels(, fontsize=8)#fontsize=65)
+            ax.set_xlim([-0.5, n_groups * n_bars - 1 - 0.5])
+            ax.set_ylim([n_groups * n_bars - 0.5, -0.5 + 1])
             ax.spines['top'].set_visible(False)
             ax.spines['right'].set_visible(False)
 
             ax.axhline(y=2.5, xmin=-0.5, xmax=2.5, color='white', linewidth=1)
             ax.axvline(x=2.5, ymin=-0.5, ymax=2.5, color='white', linewidth=1)
             for vertices in vertexlist:
-                pc = collections.PolyCollection((vertices,), color='white', edgecolor="none")
+                pc = collections.PolyCollection(
+                    (vertices,), color='white', edgecolor="none")
                 ax.add_collection(pc)
 
-            ax.add_patch(Rectangle((-2.5, 6.9), 1, 1, fill='black', color='black', alpha=1, clip_on=False))
-            ax.text(-4,9.4, 'Significant difference \n(two-sided McNemar test, \nexpected FDR=0.05)', fontsize=8, horizontalalignment='left', verticalalignment='center')
-            #plt.show()
+            ax.add_patch(Rectangle((-2.5, 6.9), 1, 1, fill='black',
+                                   color='black', alpha=1, clip_on=False))
+            ax.text(-4, 9.4, 'Significant difference \n(two-sided McNemar \
+                test, \nexpected FDR=0.05)',
+                    fontsize=8, horizontalalignment='left',
+                    verticalalignment='center')
+            # plt.show()
             plt.savefig(filename)
 
-
-
-            fig, ax = plt.subplots(figsize=(12,12))
+            fig, ax = plt.subplots(figsize=(12, 12))
             ax.matshow(significance_table, cmap='Greys')
-            #ax.set_title("McNemar's Test {} {}".format(PERCENTAGE[i],h))
-            #ax.set_xticklabels([''] + [arch[:-1] for arch in ARCHARRAY],fontsize=65)
-            ax.tick_params(labelbottom='on',labeltop='off', top='off', right='off')
-            #ax.set_yticklabels([arch[:-1] for arch in ARCHARRAY],fontsize=65)
-            ax.set_xlim([-0.5, n_groups*n_bars-1-0.5])
-            ax.set_ylim([n_groups*n_bars-0.5, -0.5+1])
+            # ax.set_xticklabels(
+            #     [''] + [arch[:-1] for arch in ARCHARRAY], fontsize=65)
+            ax.tick_params(labelbottom='on', labeltop='off',
+                           top='off', right='off')
+            # ax.set_yticklabels(
+            #     [arch[:-1] for arch in ARCHARRAY], fontsize=65)
+            ax.set_xlim([-0.5, n_groups * n_bars - 1 - 0.5])
+            ax.set_ylim([n_groups * n_bars - 0.5, -0.5 + 1])
             ax.spines['top'].set_visible(False)
             ax.spines['right'].set_visible(False)
             ax.axhline(y=2.5, xmin=-0.5, xmax=2.5, color='white')
             ax.axvline(x=2.5, ymin=-0.5, ymax=2.5, color='white')
             for vertices in vertexlist:
-                pc = collections.PolyCollection((vertices,), color='white', edgecolor="none")
+                pc = collections.PolyCollection(
+                    (vertices,), color='white', edgecolor="none")
                 ax.add_collection(pc)
-            #ax.set_ylabel('Network Architecture')
+            # ax.set_ylabel('Network Architecture')
         plt.tight_layout()
-        plt.savefig(filename.rsplit('.',1)[0] + '_sigtable.pdf')
-
+        plt.savefig(filename.rsplit('.', 1)[0] + '_sigtable.pdf')
 
 
 # TODO: Think about how to implement this, low priority
@@ -477,37 +507,12 @@ if __name__ == '__main__':
 
     ess_coll._plot_barplot_comparison(
         ess_coll.path_to_experiment +
-            '/visualization/{}_comparision.pdf'.format(
+        '/visualization/{}_comparision.pdf'.format(
             ess_coll.path_to_experiment.rsplit('/')[-1]))
     ess_coll.write_to_file(
         ess_coll.path_to_experiment +
         '/data/{}.pkl'.format(
             ess_coll.path_to_experiment.rsplit('/')[-1]))
-
-
-
-
-#
-# HOT_ENC = ["nhot"]
-# HOT_ENC = ["onehot"]
-#
-# HOT_LABELS = ["_labels"]
-#
-# PERCENTAGE = ["20p", "40p", "60p", "80p"]
-# PERCENTAGE = ["Xp"]
-# BATCHNORM = ["BN"]
-# INPUT = ["monocular", "binocular"]# ["monocular", "binocular", "binocular"] #["monocular", "monocular", "binocular", "binocular"]
-#
-# ARCHARRAY = ['B0', 'BF0', 'BK0', 'BT3', 'BL3', 'BLT3']
-# ARCHNAMES = ['B0', 'B-F0', 'B-K0', 'BT3', 'BL3', 'BLT3']
-# COLOR = ['grayscale','grayscale']
-# #ARCHARRAY = ['B0']
-# OCCARRAY = [1]
-# ITERATIONS = 5
-#
-# evaluation_iteration = 1
-#ITERATIONS = 1
-
 
 # # save output of boolean comparison
 # boolfile = open(WRITER_DIRECTORY + 'checkpoints/' +
