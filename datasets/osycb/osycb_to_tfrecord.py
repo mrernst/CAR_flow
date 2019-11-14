0b1a37de6f4b27fe1b9bf0c775a5fcbd235226b9
Markus Ernst <markusrolandernst@t-online.de>
2019-11-14 14:03:36 +0100

    process segmaps only for tfrecord production

--- datasets/osycb/osycb_to_tfrecord.py
+++ datasets/osycb/osycb_to_tfrecord.py
@@ -480,12 +480,12 @@ def _process_image_files_batch(coder, thread_index, ranges, name, filenames,
             image_buffer_l, height, width = _process_image(filename_l, coder)
             image_buffer_r, _, _ = _process_image(filename_r, coder)
 
-            # process segmentation_maps
-            seg_buffer_l, seg_buffer_r, height, width = _process_segmentation_map(filename_l, height, width, coder)
-
             if FLAGS.export:
                 _write_to_file(image_buffer_l, image_buffer_r, label, counter)
             else:
+                # process segmentation_maps
+                seg_buffer_l, seg_buffer_r, height, width = _process_segmentation_map(filename_l, height, width, coder)
+
                 #example = _convert_to_example(filename_l, image_buffer_l, filename_r, image_buffer_r, label,
                 #                              text, occ1_text, occ2_text, occ3_text, occ1_label, occ2_label, occ3_label, height, width)
                 example = make_tf_example(image_buffer_l, image_buffer_r, (label, occ1_label, occ2_label, occ3_label), occ_left, occ_right, seg_buffer_l, seg_buffer_r)
